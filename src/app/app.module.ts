ae9254afa5ed3d102288d64a181eff3ad14169d6
Diego Pagini <diegopagini777@gmail.com>
2021-01-03 15:46:29 -0300

    assets agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroeComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
